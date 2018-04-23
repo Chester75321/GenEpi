0e193f5696e74c28fb4325f4bfe7a66590ddc06d
Chester75321 <chester75321@gmail.com>
2018-04-23 05:21:42 -0700

    skew

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open('README.md') as f:
 
 setup(
     name = 'genepi',
-    version = '1.0.3',
+    version = '1.0.4',
     description = 'A package for detecting epsitasis by machine learning',
     long_description=long_description,
     long_description_content_type='text/markdown',
