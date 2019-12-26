4e40e56e6a967dbb6a3964c6261125bfc0bf09b2
chester <chester@ailabs.tw>
2019-12-26 15:34:40 +0800

    GENEPI2.0.8

--- genepi/AppGenEpi.py
+++ genepi/AppGenEpi.py
@@ -577,7 +577,7 @@ class Ui_AppGenEpi(object):
                 list_line = [QtGui.QStandardItem(field) for field in list_line]
                 self.model.appendRow(list_line)
         self.tv_result.setSortingEnabled(True)
-        self.tv_result.setStyleSheet("")
+        self.tv_result.setStyleSheet("border: none;")
 
         ### move cursor to the end of console
         cursor = self.tx_console.textCursor()
